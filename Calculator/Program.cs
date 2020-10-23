ad51951ac2e8ddf8218dab5ebbfbf006ac1cb53e
Alexandre Miranda <alexandreomiranda@outlook.com>
2020-10-23 11:49:48 +0100

    ui showing to get the inputs and show total

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,7 +6,26 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
+
+            float price;
+            int quantity;
+
+            Console.WriteLine("Enter the price and then press Enter");
+            price = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the quantity and then press Enter");
+            quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Your price: {price} and quantity {quantity}");
+
+            OrderCalculator.Price = price;
+            OrderCalculator.Quantity = quantity;
+
+            var result = OrderCalculator.CalculateOrderTotal();
+
+            Console.WriteLine($"Total: {result}");
+            Console.WriteLine();
         }
     }
 }
